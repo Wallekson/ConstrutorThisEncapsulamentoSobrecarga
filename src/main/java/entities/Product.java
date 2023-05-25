a28bba2adea3fff53a1fe94ab4a9c20a38997a3d
Wallekson <walisoncastor@gmail.com>
2023-05-25 06:56:43 -0300

    Add Sobrecarga e Encapsulamento

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,15 +1,42 @@
 package entities;
 
 public class Product {
-    public String name;
-    public double price;
-    public int quantity;
+    private String name; // por convenção os atributos dos Objetos devem ser declarados como private
+    private double price;
+    private int quantity;
+
+    public Product(){ // Construtor padrão
+
+    }
 
     public Product(String name, double price, int quantity){
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
+
+    public Product(String name, double price){ // Sobrecarga - dessa forma quantity inicia com 0
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public int getQuantity(){ // nesse caso não iremos permitir a alteração da quantidade, apenas é possivel
+        return quantity; // através do método de adicionar ou remover quantidade
+    }
+
 
     public double totalValueInStock() {
         return price * quantity;
